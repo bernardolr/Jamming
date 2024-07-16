5578b6149c490cc0d3efd5095dde66b38176626f
Bernardo Lopez Rosso <bernardolr17@gmail.com>
2024-07-16 00:34:38 -0600

    Gral layout modification + a bit greener :)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <header className="App-Header">
-        
+        <nav>Spotify Playlist</nav>
         
       </header>
       <div  className='Search-Bar'>
